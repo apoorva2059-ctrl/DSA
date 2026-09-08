27e2e84fff83aa620e8774358a0a1696fc565e61
Apoorva Gupta <apoorva2059@gmail.com>
2026-09-08 22:54:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Swap Adjacent in Array/swap-adjacent-in-array.java
+++ Difficulty: Easy/Swap Adjacent in Array/swap-adjacent-in-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public void swapElements(int[] arr) {
         // code here
+        int a[] = new int[2];
         for(int i=0; i<arr.length-2; i++)
         {
             int temp = arr[i];
