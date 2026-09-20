4610f1147a6475ba7055214f1f61531d5ae16e5f
Apoorva Gupta <apoorva2059@gmail.com>
2026-09-20 21:33:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse a String/reverse-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static String reverseString(String s) {
+        // code here
+        String reverse = "";
+        for(int i=s.length()-1; i>=0; i--)
+        {
+            reverse = reverse + s.charAt(i);
+        }
+        return reverse;
+    }
+}
