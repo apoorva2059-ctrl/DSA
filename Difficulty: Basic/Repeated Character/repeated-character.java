760ce796142fa682c7c94daadb14e6bd604e4283
Apoorva Gupta <apoorva2059@gmail.com>
2026-09-20 22:32:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated Character/repeated-character.java
@@ -0,0 +1,17 @@
+class Solution {
+    char firstRep(String S) {
+        // your code here
+        int count = 0;
+        for(int i=0; i<S.length(); i++)
+        {
+          for(int j= i+1; j<S.length(); j++)
+          {
+            if(S.charAt(i) == S.charAt(j))
+              {
+                return S.charAt(i);
+              }
+          }
+        }
+        return '#';
+    }
+}
